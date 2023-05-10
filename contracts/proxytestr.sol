a26a2fdc747bbe1e17a89138e07c62820930e446
CosimoRicciardi <120040607+CosimoRicciardi@users.noreply.github.com>
2023-05-10 12:15:34 +0200

    trying to figure out the proxyfactory

--- contracts/proxytestr.sol
+++ contracts/proxytestr.sol
@@ -1,28 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.0;
+pragma solidity ^0.8.0;
 
-// import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+contract MultisigWalletFactory  {
+   address public implementationContract;
 
-// contract MultiSigWalletFactory is Initializable {
-//     address[] public owners;
-//     uint256 public numConfirmationsRequired;
-//     uint256 public walletId;
+  constructor(address _implementationContract) {
+    implementationContract = _implementationContract;
+  }
+    event ProxyCreated(address proxy);
+ function deployMinimal(address _logic, bytes memory _data) public returns (address proxy) {
+    // Adapted from https://github.com/optionality/clone-factory/blob/32782f82dfc5a00d103a7e61a17a5dedbd1e8e9d/contracts/CloneFactory.sol
+    bytes20 targetBytes = bytes20(_logic);
+    assembly {
+      let clone := mload(0x40)
+      mstore(clone, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
+      mstore(add(clone, 0x14), targetBytes)
+      mstore(add(clone, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+      proxy := create(0, clone, 0x37)
+    }
+    
+    emit ProxyCreated(address(proxy));
 
-//     function initialize(address[] memory _owners, uint256 _numConfirmationsRequired, uint256 _walletId) public initializer {
-//         require(_owners.length > 0, "MultisigWallet: Owners array should not be empty");
-//         require(_numConfirmationsRequired > 0, "MultisigWallet: Minimum confirmations required should be greater than 0");
-//         require(_numConfirmationsRequired <= _owners.length, "MultisigWallet: Required confirmations should not exceed number of owners");
-
-//         for (uint256 i = 0; i < _owners.length; i++) {
-//             require(_owners[i] != address(0), "MultisigWallet: Owner address should not be 0");
-//             for (uint256 j = i + 1; j < _owners.length; j++) {
-//                 require(_owners[i] != _owners[j], "MultisigWallet: Owner addresses should be unique");
-//             }
-//         }
-
-//         owners = _owners;
-//         numConfirmationsRequired = _numConfirmationsRequired;
-//         walletId = _walletId;
-//     }
-// }
+    if(_data.length > 0) {
+         bytes memory data = abi.encodeWithSignature("initialize(address[] memory,uint,uint)", "");
+      (bool success,) = proxy.call(data);
+      require(success);
+    }    
+  }
+    
+    
+  function createWallet(bytes memory _data) public returns (address){
+    address proxy = deployMinimal(implementationContract, _data);
+    return proxy;
+  }
+}
 
